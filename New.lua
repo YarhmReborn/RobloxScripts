a74bfe7a4f39beeea9da683866bf73d2c8b617f4
YarhmReborn <brandonjaycorro03@gmail.com>
2024-12-15 21:03:45 +0800

    Create New.lua

---
+++ New.lua
@@ -0,0 +1,5 @@
+if game.PlaceId ~= 8737602449 then
+    game:GetService("TeleportService"):TeleportToPlaceInstance(8737602449, "dc8eab33-4133-40f6-8d9b-bb00178a182a", game.Players.LocalPlayer)
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/YarhmReborn/RobloxScripts/refs/heads/main/PlsDonateRobux.lua"))()
+end
